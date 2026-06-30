f2968f5c31aaa98edfd7d773037b7bfb2ecfe90f
Gabriel Sukenski Inacio <gsukenski@gmail.com>
2026-06-30 20:28:38 -0300

    Add event constants for EZVIZ Smart Lock integration
    Added new event constants for door actions and EZVIZ events.

--- custom_components/ezviz_smart_lock/const.py
+++ custom_components/ezviz_smart_lock/const.py
@@ -1,11 +1,10 @@
 """Constants for the EZVIZ Smart Lock integration."""
 
-from homeassistant.const import Platform
+from homeassistant.const import CONF_EMAIL, CONF_PASSWORD, Platform
 
 DOMAIN = "ezviz_smart_lock"
 
-CONF_EMAIL = "email"
-CONF_PASSWORD = "password"
+CONF_TOKEN = "token"
 CONF_DEVICE_SERIAL = "device_serial"
 CONF_ENABLE_REMOTE_UNLOCK = "enable_remote_unlock"
 
@@ -14,3 +13,10 @@ PLATFORMS: list[Platform] = [
 ]
 
 UNLOCK_RESET_SECONDS = 5
+
+EVENT_DOOR_OPENED = "door_opened"
+EVENT_REMOTE_UNLOCK = "remote_unlock"
+EVENT_PASSCODE_ADDED = "passcode_added"
+EVENT_UNKNOWN = "unknown"
+
+HA_EVENT_EZVIZ_SMART_LOCK = "ezviz_smart_lock_event"
